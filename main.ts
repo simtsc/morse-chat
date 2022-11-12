a705dd4cacecbe4119f4b2f585194461f9b48d61
simtsc <2539637+simtsc@users.noreply.github.com>
2022-11-12 11:59:41 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,47 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendNumber(1)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
